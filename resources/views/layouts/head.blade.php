778d2b95a0547e164898874b9e59b80fe8c11b0d
Jimmy Febriadi <jimmyfreddi@gmail.com>
2024-03-17 15:32:10 +0700

    not load

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -8,11 +8,11 @@
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- DataTables -->
-  <link rel="stylesheet" href="{{asset('adminLTE3/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
-  <link rel="stylesheet" href="{{asset('adminLTE3/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('adminLTE3/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('adminLTE3/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('adminLTE3/dist/css/adminlte.min.css')}}">
-  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('adminLTE3/dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('css/style.css')}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
